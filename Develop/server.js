7fce8307d28e38b20911cbd5a24a24e3471a2f0f
Gaye Proctor <gayebroni@yahoo.com>
2021-07-31 19:50:21 -0400

    create and assign noteapp to express, create and test routes and create a route for notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,30 @@
-const fs = require('fs')
-const express = require('express')
+const fs = require("fs")
+const express = require("express")
+const noteapp = express();
+noteapp.use(express.static('public'));
+
+
+noteapp.get("/home", (req, res)=> {
+    res.send("HI!");
+});
+
+noteapp.get("/notes", (req,res)=> {
+    res.sendFile("notes.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+noteapp.listen(3002, () => {
+    console.log("API server now on port!");
+});
