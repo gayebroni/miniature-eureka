6ef147319eaef0c5eea63cef5cc7cdaa0c8bbc3c
Gaye Proctor <gayebroni@yahoo.com>
2021-08-04 01:48:11 -0400

    start code to set up route on server to accept data to be used or stored server-side

--- Develop/server.js
+++ Develop/server.js
@@ -1,27 +1,31 @@
+//Dependencies
 const fs = require("fs")
+const path = require("path")
 const express = require("express")
+
+// This creates an express server
 const noteapp = express();
-noteapp.use(express.static('public'));
 
 
 noteapp.get("/home", (req, res)=> {
     res.send("HI!");
 });
 
+
 noteapp.get("/notes", (req,res)=> {
-    res.sendFile("notes.html");
+    res.sendFile(path.resolve() + "/public/notes.html");
+});
+noteapp.get("/", (req,res)=> {
+    res.sendFile(path.resolve() + "/public/index.html");
 });
 
+noteapp.get("/api/notes", (req,res)=> 
+{
+    const data = fs.readFileSync(path.resolve() + "/db/db.json", "utf8");
+    res.json(data);
+});
 
-
-
-
-
-
-
-
-
-
+noteapp.post("/api/notes", (req, res) => {});
 
 
 
